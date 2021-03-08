7dcc8000fb815a654bd889c68ed32c6d5fb6152e
skibina1 <askiba1705@gmail.com>
2021-03-08 22:08:57 +0100

    add eslint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
+	return (
+		<div className="App">
       Siema
-    </div>
-  );
+		</div>
+	)
 }
 
-export default App;
+export default App
