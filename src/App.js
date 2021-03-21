89c94eb2dd806e4485d552fb4abebe633835efc1
skibina1 <askiba1705@gmail.com>
2021-03-21 23:41:41 +0100

    add blueprint

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
 import { getData } from './util'
+import { Select } from '@blueprintjs/select'
+import { Button, MenuItem, Label } from '@blueprintjs/core'
+
+const teams = [
+  { team: 'Black Widow' },
+  { team: 'Young Lions'}
+]
 
 function App() {
   const [text, setText] = useState('Test')
 
-  useEffect( async () => {
-    let response = await getData('Black Widow', 'Young Lions')
-    console.log(response)
-    setText(JSON.stringify(response))
-  }, [])
+  // useEffect( async () => {
+  //   let response = await getData('Black Widow', 'Young Lions')
+  //   console.log(response)
+  //   setText(JSON.stringify(response))
+  // }, [])
+
+  function itemRenderer(item, { handleClick }) {
+    return (
+      <MenuItem
+        key={item.team}
+        label={item.team}
+        text={item.team}
+        onClick={handleClick}
+        shouldDismissPopover={true}
+      />
+    )
+  }
+
+  function handleClick(item) {
+    console.log(item)
+  }
 
   return (
     <div className="App">
-      {text}
+      <div>
+        {text}
+      </div>
+      <div>
+        <Select 
+          items={teams}
+          filterable={false}
+          itemRenderer={itemRenderer}
+          onItemSelect={handleClick}
+        >
+          <Button
+            text={teams[0].team}
+            rightIcon="caret-down"
+          />
+        </Select>
+      </div>
+      <div>
+        <Label>Label B</Label>
+      </div>
     </div>
   )
 }
--- src/util.js
+++ src/util.js
@@ -1,4 +1,4 @@
-const HOST = 'http://localhost:3002/'
+const HOST = 'https://team-comparison-db-api.herokuapp.com/'
 
 async function getData(team1, team2) {
   let response = await fetch(HOST + 'matches/select?team1='+team1+'&team2='+team2)
