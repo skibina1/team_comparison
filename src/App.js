f9bd8559b648255ab81525c2a75f621cb759f2a8
Adrian Skibinski <adrian.skibinski@si4.pl>
2021-04-19 00:06:44 +0200

    handle progress bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
 import SelectTeam from './components/SelectTeam'
+import { ProgressBar, ProgressMeter } from './components/ProgressBar'
 import { calculateTeamStats, getData } from './util'
 
 const teams = [
   { team: 'Black Widow' },
   { team: 'Young Lions'},
-  { team: 'Nie wybieraj'}
+  { team: 'Nie wybieraj'},
+  { team: 'Top Smokers Club Reprezentant'}
 ]
 
 function App() {
+  // let sum = 0, teamAwin = 0, teamBwin = 0
   const [teamA, setTeamA] = useState(null)
   const [teamB, setTeamB] = useState(null)
   const [response, setResponse] = useState(null)
+  let [results, setResults] = useState({
+    sum: 0,
+    teamAwin: 0,
+    teamBwin: 0
+  })
 
   useEffect( async () => {
     if(teamA && teamB){
       const {team1result, team2result, draw} = calculateTeamStats(await getData(teamA, teamB), teamA, teamB)
+      // teamAwin = team1result
+      // teamBwin = team2result
+      // sum = teamAwin + teamBwin
       setResponse(`${teamA}: ${team1result}, ${teamB}: ${team2result}, draw: ${draw}, `)
+      setResults(results = {
+        sum: team1result + team2result,
+        teamAwin: team1result,
+        teamBwin: team2result
+      })
     }
   }, [teamA, teamB])
   
@@ -38,17 +54,12 @@ function App() {
         {teamB}
       </div> 
       <div>
-        <ProgressBar/>
+        <ProgressBar>
+          <ProgressMeter value={results.teamAwin/results.sum} color='orange' />
+          <ProgressMeter value={1 - results.teamAwin/results.sum} color='blue' />
+        </ProgressBar>
         {response}
       </div>    
-    </div>
-  )
-}
-
-const ProgressBar = (/*{ children }*/) => {
-  return (
-    <div className="progress-bar" >
-      {/* {children} */}
     </div>
   )
 }
---
+++ src/components/ProgressBar.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+const ProgressBar = ({ children }) => {
+  return (
+    <div className="progress-bar" >
+      {children}
+    </div>
+  )
+}
+
+const ProgressMeter = ({ value = 0, color }) => {
+  const newValue = !isNaN(value) ? value * 100 : 0
+  const percent = Math.round(newValue)
+  const width = (newValue > 100 ? 100 : newValue) + '%'
+
+  return (
+    <div className='progress-meter' style={{ backgroundColor: color, width }}>{percent}%</div>
+  )
+}
+
+ProgressMeter.propTypes = {
+  value: PropTypes.number,
+  color: PropTypes.string
+}
+
+ProgressBar.propTypes = {
+  children: PropTypes.element
+}
+
+export { ProgressBar, ProgressMeter }
