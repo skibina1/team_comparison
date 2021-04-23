883db91737a2467ec23c8450bb675e7c69ddbccd
Adrian Skibinski <adrian.skibinski@si4.pl>
2021-04-23 21:43:01 +0200

    remove unused code

--- src/App.js
+++ src/App.js
@@ -7,12 +7,10 @@ import { calculateTeamStats, getData } from './util'
 const teams = [
   { team: 'Black Widow' },
   { team: 'Young Lions'},
-  { team: 'Nie wybieraj'},
   { team: 'Top Smokers Club Reprezentant'}
 ]
 
 function App() {
-  // let sum = 0, teamAwin = 0, teamBwin = 0
   const [teamA, setTeamA] = useState(null)
   const [teamB, setTeamB] = useState(null)
   const [response, setResponse] = useState(null)
@@ -30,8 +28,8 @@ function App() {
       setResults(results = {
         sum: team1result + team2result,
         teamAwin: team1result,
-        teamBwin: 1,
-        draw: 1
+        teamBwin: team2result,
+        draw: draw
       })     
     }
   }, [teamA, teamB])
@@ -54,7 +52,7 @@ function App() {
       </div> 
       <div>
         <ProgressBar>
-          <ProgressMeter value={results.teamAwin/results.sum} color='orange' />
+          <ProgressMeter value={results.teamAwin/results.sum} color='orange'/>
           <ProgressMeter value={results.draw/results.sum} color='#ced2d6' />
           <ProgressMeter value={1 - (results.teamAwin+results.draw)/results.sum} color='blue' />
         </ProgressBar>
