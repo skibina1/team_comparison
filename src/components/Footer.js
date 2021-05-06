d4e939e184e557f8f203abfabfd0f5f3f06947a4
skibina1 <askiba1705@gmail.com>
2021-05-07 00:58:51 +0200

    change github link repo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => (
   <div className="footer">
     <p>Speedway Turbo Sliders & <a className="link" href="http://www.sts-pl.eu" >www.sts-pl.eu</a></p>
     <a href="https://www.facebook.com/stspl.eu/"><img className="logoSize" src={logo} alt="Facebook"/></a>
-    <a href="https://github.com/skibina1/podaj_trzy"><img className="logoSize" src={gitHub} alt="GitHub"></img></a>
+    <a href="https://github.com/skibina1/team_comparison"><img className="logoSize" src={gitHub} alt="GitHub"></img></a>
   </div>
 )
 
