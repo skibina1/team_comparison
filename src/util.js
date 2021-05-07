6d8cf6a1687aa552981f863cd530fd96cc0a4ba4
skibina1 <askiba1705@gmail.com>
2021-05-07 22:04:34 +0200

    add conversion for percentage sign

--- src/util.js
+++ src/util.js
@@ -1,6 +1,9 @@
 const HOST = 'https://team-comparison-db-api.herokuapp.com/'
 
 async function getData(team1, team2) {
+  team1 = team1.replace('%', '%25')
+  team2 = team2.replace('%', '%25')
+  console.log(team1, team2)
   let response = await fetch(HOST + 'matches/select?team1='+team1+'&team2='+team2)
     .then(response => response.json())
     .then(res => { return res })
